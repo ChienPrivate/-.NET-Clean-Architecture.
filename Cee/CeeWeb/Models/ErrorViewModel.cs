98b839bb5a790a8d9cc2f0359149f5b344908e05
Cee <chiennguyen2841998@gmail.com>
2025-02-11 15:12:18 +0700

    Initial commit

---
+++ Cee/CeeWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CeeWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
