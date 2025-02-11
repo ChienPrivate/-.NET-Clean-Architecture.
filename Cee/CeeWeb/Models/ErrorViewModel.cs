a50aa184ff681ed3bd20f8838ecb606b0db0a2e1
Cee <chiennguyen2841998@gmail.com>
2025-02-11 19:31:16 +0700

    build layers and add first model to Domain layer

--- Cee/CeeWeb/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CeeWeb.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
---
+++ WhiteLagoon/WhiteLagoon.Web/Migrations/20250211095655_modify_villa_table.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WhiteLagoon.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class modify_villa_table : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ImgageUrl",
+                table: "Villas",
+                newName: "ImageUrl");
+
+            migrationBuilder.AddColumn<double>(
+                name: "Price",
+                table: "Villas",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Villas");
+
+            migrationBuilder.RenameColumn(
+                name: "ImageUrl",
+                table: "Villas",
+                newName: "ImgageUrl");
+        }
+    }
+}
